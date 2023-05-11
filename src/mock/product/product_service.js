dd0697f631235bc9a68c35e4e0bffc0bf3181519
takethisbus <takethisbus@gmail.com>
2023-05-11 22:35:40 +0900

    Add: stub version test case

---
+++ src/mock/product/product_service.js
@@ -0,0 +1,15 @@
+const ProductClient = require("./product_client");
+
+class ProductService {
+  constructor(ProductClient) {
+    this.productClient = ProductClient;
+  }
+
+  fetchAvailableItems() {
+    return this.productClient
+      .fetchItems()
+      .then((items) => items.filter((item) => item.available));
+  }
+}
+
+module.exports = ProductService;
